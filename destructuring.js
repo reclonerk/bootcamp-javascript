b96725c479d887c3b62254130ee01988354f7d8e
Rahul Kumar <rkauraiya1167@gmail.com>
2021-08-30 14:21:19 +0530

    destructuring added

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const Employee = {
+    name : "Rahul",
+    lanId : "sdfdgd",
+    designation : "data engineer"
+}
+
+
+// console.log(Employee.lanId)
+
+const {lanId, designation} = Employee
+
+console.log(designation)
